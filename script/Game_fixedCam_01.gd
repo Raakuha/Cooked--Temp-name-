67eb7e57257dc0abb00f7e125d5cbe5514a07dc7
Raakuha <rakharamadh12345@gmail.com>
2026-07-28 17:02:26 +0700

    Set up baked navigation mesh with test obstacle

--- script/Game_fixedCam_01.gd
+++ script/Game_fixedCam_01.gd
@@ -15,7 +15,7 @@ func _ready() -> void:
 func _on_player_arrived_at_target() -> void:
 	if movement_step == 1:
 		player.move_to_target(target_marker_a.global_position)
-		movement_step = 2
+		movement_step = 2	
 	elif movement_step == 2:
 		print("Player berhasil gerak ke kulkas lur")
 	
