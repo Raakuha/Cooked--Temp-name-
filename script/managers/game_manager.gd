9c7aeb198930e2fbefeeecb94390af1b2fd52d3b
ILOLB4suyo <167866624+ILOLB4suyo@users.noreply.github.com>
2026-08-29 21:30:06 +0800

    update_Gameroot

--- script/managers/game_manager.gd
+++ script/managers/game_manager.gd
@@ -384,7 +384,7 @@ func start_day() -> void:
 		orders_completed_today
 	)
 
-	day_manager.start_day(4)
+	day_manager.start_day(7)
 
 func _on_day_started(day: int) -> void:
 	print("GameManager memulai Day ", day)
